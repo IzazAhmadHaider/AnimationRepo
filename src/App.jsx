cce2c12cdbac87b0032c2ebe57b9ebd9bbb02fdf
izaz <izazahmadhaider0@gmail.com>
2025-01-30 12:22:50 +0500

    added router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import ReactAnimation from './components/React_Animations'
 import './App.css'
+import { Route, Routes } from 'react-router'
+import Home from './components/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <ReactAnimation/>
+
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="reactanimation" element={<ReactAnimation />} />
+      </Routes>
     </>
   )
 }
